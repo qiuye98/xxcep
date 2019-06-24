d5ab2b6cb5e8ae95666fb4dfd177594d0429514c
S16203130 <425044630@qq.com>
2019-06-24 17:29:36 +0800

    0624 test

--- src/main/java/com/xxcep/demo/controller/HomeworkController.java
+++ src/main/java/com/xxcep/demo/controller/HomeworkController.java
@@ -1,6 +1,7 @@
 package com.xxcep.demo.controller;
 
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.stereotype.Controller;
@@ -16,6 +17,23 @@ import org.springframework.stereotype.Controller;
 @Controller
 @RequestMapping("/homework")
 public class HomeworkController {
+
+    @GetMapping("/student/detail")
+    public String toStudentHomeworkDetail() {
+        return "student/homework-detail";
+    }
+
+    @GetMapping("/teacher/homework")
+    public String toStudentHome() {
+        return "teacher/homework";
+    }
+
+
+    @GetMapping("/teacher/detail")
+    public String toTeacherHomeworkDetail() {
+        return "teacher/homework-detail";
+    }
+
 
 }
 
